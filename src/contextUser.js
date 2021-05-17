5a5142b71c8219fac9910975a934727b6f587049
maryam <maryum_shahnawaz@yahoo.com>
2021-05-17 16:31:45 -0400

    created globalMinutes,globalHours functions inside contextUser.js

--- src/contextUser.js
+++ src/contextUser.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
 // useContext hook take a context object as an argument
 import { createContext, useState } from 'react';
 // we have access to two components consumer and provider we only use provider inside createContext we have provider and consumer it is stored in a UserContext if we want to get access to it we will call <UserContext.provider>
+export const UserContext = createContext({});
 
-const contextUser = () => {
+const contextUser = (props) => {
   const [globalMinute, setGlobalminute] = useState(null);
   const [globalHour, setGlobalHour] = useState(null);
-  return (
-    <div>
 
-    </div>
+  const storeMinutes = (userMinutesInput) => {
+    setGlobalminute(userMinutesInput);
+  }
+
+  const storeHours = (userHoursInput) => {
+    setGlobalHour(userHoursInput);
+  }
+
+  return (
+    <UserContext.provider value={{ globalMinute, globalHour, storeMinutes, storeHours }}>
+      {/* we need to wrap our whole application here to pass the data */}
+      {props.children}
+    </UserContext.provider>
   )
 }
 
