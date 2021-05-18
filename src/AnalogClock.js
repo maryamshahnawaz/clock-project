2d608475f705aaf29ccd028ba3b2120fcaaa70a8
maryam <maryum_shahnawaz@yahoo.com>
2021-05-17 21:22:27 -0400

    clean and update the code on Analog App styles and context files

--- src/AnalogClock.js
+++ src/AnalogClock.js
@@ -1,41 +1,35 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import DigitalClock from "./DigitalClock";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Clock from "./Clock";
 import { UserContext } from "./context";
-
+import DigitalClock from "./DigitalClock";
 const AnalogClock = ({ timeUpdate, format }) => {
-
-  //analog clocks hours minutes and seconds
-  const [hours, setHours] = useState('');
-  const [minutes, setMinutes] = useState('');
-  const [seconds, setSeconds] = useState('');
+  const [hours, setHours] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [seconds, setSeconds] = useState("");
 
   const ctx = React.useContext(UserContext);
 
-  // user input values in state
+  ////////////////////////////////
+  //user inputed values
   const [cusMin, setCusMin] = useState(null);
   const [cusHour, setCusHour] = useState(null);
 
-
-  //set interval for analog
   let clockInterval;
   useEffect(() => {
+    // eslint-disable-next-line
     clockInterval = setInterval(handleDate, 1000);
   }, [clockInterval]);
+  // eslint-disable-next-line
+  useEffect(() => () => clearInterval(clockInterval), []);
 
-  // clear interval
-  useEffect(() => {
-    return (() => {
-      clearInterval(clockInterval)
-    })
-  });
-
-
+  ////////////////////////////////
+  //   const getHrFromLocal = localStorage.getItem("hr");
+  //   const getMinFromLocal = localStorage.getItem("min");
 
   const secondsStyle = {
-    tranform: `rotate(${seconds * 6}deg)`,
+    transform: `rotate(${seconds * 6}deg)`,
   };
-
   const minutesStyle = {
     transform: `rotate(${(ctx.cusMins !== null && ctx.cusMins * 6) ||
       cusMin ||
@@ -43,7 +37,6 @@ const AnalogClock = ({ timeUpdate, format }) => {
       minutes * 6
       }deg)`,
   };
-
   const hoursStyle = {
     transform: `rotate(${
       // eslint-disable-next-line
@@ -62,18 +55,13 @@ const AnalogClock = ({ timeUpdate, format }) => {
     (previousHour === null && Number(timeUpdate && timeUpdate.hr) + 1) ||
     previousHour + 1;
 
-
-  // Time function
   const handleDate = () => {
-    // instantiate date from browser
-    const date = new Date();
-    date.setHours(date.getHours());
-    let hours = formatTime(date.getHours());
+    const date = new Date(); // date instance- Javascript date
+    date.setHours(date.getHours()); // setHours is builtin : built in to Javascript - only for changing the time: different countries - time
+    let hours = formatTime(date.getHours()); //format is adding 0
     let minutes = formatTime(date.getMinutes());
     let seconds = formatTime(date.getSeconds());
-
-    //setting analog time state to get time on clock
-    setHours(hours);
+    setHours(hours); //setting states
     setMinutes(minutes);
     setSeconds(seconds);
 
@@ -99,67 +87,62 @@ const AnalogClock = ({ timeUpdate, format }) => {
       }
     }
   };
-  //formatTime function if time less then 10 it will add 0 before it
 
   const formatTime = (time) => {
     return time < 10 ? `0${time}` : time;
   };
-
-
   return (
-    <>
-      <div className="clock">
-        <div className="analog-clocks">
-          <div className="dial seconds" style={secondsStyle} />
-          <div className="dial minutes" style={minutesStyle} />
-          <div className="dial hours" style={hoursStyle} />
-          <span className="number-twelve">
-            <small style={{ fontSize: "10px" }}>12</small>
-          </span> <span className="number-twelve">
-            <small style={{ fontSize: "10px" }}>12</small>
-          </span>
-          <span className="number-one">
-            <small style={{ fontSize: "10px" }}>1</small>
-          </span>
-          <span className="number-two">
-            <small style={{ fontSize: "10px" }}>2</small>
-          </span>
-          <span className="number-three">
-            <small style={{ fontSize: "10px" }}>3</small>
-          </span>
-          <span className="number-four">
-            <small style={{ fontSize: "10px" }}>4</small>
-          </span>
+    <div className={"clocks"}>
+      <div className={"analog-clocks"}>
+        <div className={"dial seconds"} style={secondsStyle} />
+        <div className={"dial minutes"} style={minutesStyle} />
+        <div className={"dial hours"} style={hoursStyle} />
 
-          <span className="number-five">
-            <small style={{ fontSize: "10px" }}>5</small>
-          </span>
-          <span className="number-six">
-            <small style={{ fontSize: "10px" }}>6</small>
-          </span>
-          <span className="number-seven">
-            <small style={{ fontSize: "10px" }}>7</small>
-          </span>
-          <span className="number-eight">
-            <small style={{ fontSize: "10px" }}>8</small>
-          </span>
-          <span className="number-nine">
-            <small style={{ fontSize: "10px" }}>9</small>
-          </span>
-          <span className={"number-ten"}>
-            <small style={{ fontSize: "10px" }}>10</small>
-          </span>
-          <span className={"number-eleven"}>
-            <small style={{ fontSize: "10px" }}>11</small>
-          </span>
-        </div>
-        <div className="digital-clock">
-          <DigitalClock timeUpdate={timeUpdate} format={format} />
-        </ div>
+        <span className="number-twelve">
+          <small style={{ fontSize: "10px" }}>12</small>
+        </span>
+        <span className="number-one">
+          <small style={{ fontSize: "10px" }}>1</small>
+        </span>
+        <span className="number-two">
+          <small style={{ fontSize: "10px" }}>2</small>
+        </span>
+        <span className="number-three">
+          <small style={{ fontSize: "10px" }}>3</small>
+        </span>
+        <span className="number-four">
+          <small style={{ fontSize: "10px" }}>4</small>
+        </span>
+        <span className="number-five">
+          <small style={{ fontSize: "10px" }}>5</small>
+        </span>
+        <span className="number-six">
+          <small style={{ fontSize: "10px" }}>6</small>
+        </span>
+        <span className="number-seven">
+          <small style={{ fontSize: "10px" }}>7</small>
+        </span>
+        <span className="number-eight">
+          <small style={{ fontSize: "10px" }}>8</small>
+        </span>
+        <span className="number-nine">
+          <small style={{ fontSize: "10px" }}>9</small>
+        </span>
+        <span className={"number-ten"}>
+          <small style={{ fontSize: "10px" }}>10</small>
+        </span>
+        <span className={"number-eleven"}>
+          <small style={{ fontSize: "10px" }}>11</small>
+        </span>
       </div>
 
-    </>
-  )
-}
+      <div className={"digital-clocks"}>
+        <DigitalClock timeUpdate={timeUpdate} format={format} />
+      </div>
+    </div>
+  );
+  //   }
+};
 
 export default AnalogClock;
+
--- src/context.js
+++ src/context.js
@@ -1,27 +1,23 @@
 // useContext hook take a context object as an argument
-import React, { createContext, useState } from 'react';
-// we have access to two components consumer and provider we only use provider inside createContext we have provider and consumer it is stored in a UserContext if we want to get access to it we will call <UserContext.provider>
+import React, { createContext, useState } from "react";
 export const UserContext = createContext({});
-
+// we have access to two components consumer and provider we only use provider inside createContext we have provider and consumer it is stored in a UserContext if we want to get access to it we will call <UserContext.provider>
 const AppContextWrapper = (props) => {
-  const [cusMins, setCusMins] = useState(null);
-  const [cusHours, setCusHours] = useState(null);
+  const [cusMins, setCusMin] = useState(null);
+  const [cusHours, setCusHour] = useState(null);
 
   const storeMin = (user) => {
-    setCusMins(user);
-  }
-
+    setCusMin(user);
+  };
   const storeHour = (token) => {
-    setCusHours(token);
-  }
+    setCusHour(token);
+  };
 
   return (
     <UserContext.Provider value={{ cusMins, cusHours, storeMin, storeHour }}>
-      {/* we need to wrap our whole application here to pass the data */}
       {props.children}
     </UserContext.Provider>
-  )
-}
-
+  );
+};
 export default AppContextWrapper;
 
